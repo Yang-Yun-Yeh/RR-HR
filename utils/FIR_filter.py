99b7952d7ca00e9618192784aa4e79c0c20de0bd
Yang Yun Yeh <lim299792458ms@g.ncu.edu.tw>
2025-02-13 08:22:26 +0800

    Add RLS method

--- utils/FIR_filter.py
+++ utils/FIR_filter.py
@@ -7,6 +7,9 @@ class FIR_filter:
         self.coefficients = _coefficients
         self.buffer = np.zeros(self.ntaps)
 
+        # RLS
+        self.P = None
+
     def filter(self, v):
         for j in range(self.ntaps-1):
             self.buffer[self.ntaps-j-1] = self.buffer[self.ntaps-j-2]
@@ -36,3 +39,14 @@ class FIR_filter:
 
         # self.coefficients = h_hat
         self.coefficients = reg.coef_
+    
+    def rls(self, alpha, delta, lam=0.9995):
+        if self.P is None:
+            self.P = delta * np.eye(self.ntaps,self.ntaps)
+
+        g = self.P @ self.buffer / (lam + self.buffer @ self.P @ self.buffer) # (m, 1)
+        g = g.reshape(-1, 1) # (m, 1)
+        x_T = self.buffer.reshape(1, -1) # (1, m)
+        self.P = self.P / lam - g @ x_T @ self.P / lam # (m, m)
+
+        self.coefficients = self.coefficients + g.reshape(-1) * alpha
--- utils/LMS_filter.py
+++ utils/LMS_filter.py
@@ -8,29 +8,33 @@ NTAPS = 100
 LEARNING_RATE = 0.001
 fnoise = 50
 
+# RLS
+delta = 1000 # 1
+lam = 0.998 # 0.9995
+
 ecg = np.loadtxt("data_test/ecg50hz.dat")
 plt.figure(1)
 plt.plot(ecg)
 
 f = FIR_filter.FIR_filter(np.zeros(NTAPS))
 
-# ls
-x = np.arange(len(ecg))
-x = np.sin(2.0 * np.pi * fnoise/fs * x)
-d = ecg
-f.ls(x, d)
-LEARNING_RATE = np.max(f.coefficients) / 1e6 # / 100
-print(f'LEARNING_RATE:{LEARNING_RATE}')
-
-# exit()
-
+# LS
+# x = np.arange(len(ecg))
+# x = np.sin(2.0 * np.pi * fnoise/fs * x)
+# d = ecg
+# f.ls(x, d)
+# LEARNING_RATE = np.max(f.coefficients) / 1e6 # / 100
+# print(f'LEARNING_RATE:{LEARNING_RATE}')
 
 y = np.empty(len(ecg))
 for i in range((len(ecg))):
     ref_noise = np.sin(2.0 * np.pi * fnoise/fs * i)
     canceller = f.filter(ref_noise)
     output_signal = ecg[i] - canceller
-    f.lms(output_signal, LEARNING_RATE)
+    # LMS
+    # f.lms(output_signal, LEARNING_RATE)
+    # RLS
+    f.rls(output_signal, delta=delta, lam=lam)
     y[i] = output_signal
 
 plt.figure(2)
